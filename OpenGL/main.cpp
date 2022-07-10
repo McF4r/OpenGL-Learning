e01b4d757c140cb39bc3bf002ea131d12c369aea
McF4r <1206030024@qq.com>
2022-07-10 19:46:15 +0800

    OpenGL环境搭建完成

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,14 +1,55 @@
-//
-//  main.cpp
-//  OpenGL
-//
-//  Created by McF4r on 2022/7/10.
-//
-
+#define GL_SILENCE_DEPRECATION ; //用于取消deprecated警告
+#include <GL/glew.h> //要放在最前面
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    //这个函数需在一个opengl rendering context被渲染之后才能被call,也就是上面这个glfwMakeContextCurrent
+    if(glewInit() != GLEW_OK)
+        std::cout << "Error" << std::endl;
+    
+    //某一个新OpenGL函数
+    unsigned int a;
+    glGenBuffers(1, &a);
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
