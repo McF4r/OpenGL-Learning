88d4c5153194d1d8b23b3baacd961c49d73da3f7
McF4r <1206030024@qq.com>
2022-07-10 13:49:59 +0800

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by McF4r on 2022/7/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
